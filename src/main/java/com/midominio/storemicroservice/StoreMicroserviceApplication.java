04ef11fcb0a2b342d1b5265e43210a07a2e2c29c
David G <davidgar8879@gmail.com>
2024-03-10 14:06:33 -0600

    Integration with Eureka client

--- src/main/java/com/midominio/storemicroservice/StoreMicroserviceApplication.java
+++ src/main/java/com/midominio/storemicroservice/StoreMicroserviceApplication.java
@@ -3,6 +3,12 @@ package com.midominio.storemicroservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Principal class in spring boot microservice.
+ *
+ * @author David G.
+ * @version 1.0-
+ */
 @SpringBootApplication
 public class StoreMicroserviceApplication {
 
