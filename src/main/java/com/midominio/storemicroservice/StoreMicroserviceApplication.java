44deeb7d6013c5a9fda19334463d84765ac58d20
David G <davidgar8879@gmail.com>
2024-03-10 12:42:08 -0600

    Inital commit

---
+++ src/main/java/com/midominio/storemicroservice/StoreMicroserviceApplication.java
@@ -0,0 +1,18 @@
+package com.midominio.storemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreMicroserviceApplication {
+
+	/**
+	 * Initializer method main.
+	 *
+	 * @param args parameters.
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(StoreMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/midominio/storemicroservice/StoreMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midominio.storemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
